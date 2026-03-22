8167dde8cb80d59ab60c2139a0c8b2e618d240b9
Aayush Raj <devxpriyanshu@gmail.com>
2026-03-22 15:01:45 +0530

    CB added

---
+++ src/main/java/com/ecom_app/product_service/Config/OpenAPIConfig.java
@@ -0,0 +1,25 @@
+package com.ecom_app.product_service.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenAPIConfig {
+    
+    @Bean
+    public OpenAPI productServiceDocsAPI(){
+        return new OpenAPI()
+                .info(new Info().title("Product Service API")
+                    .description("this is the REST API for product servie")
+                    .version("v0.0.1")
+                    .license(new License().name("Satan Verified")))
+                .externalDocs(new ExternalDocumentation()
+                    .description("you can refer for further more")
+                    .url("https://google.com"));
+    }
+}
